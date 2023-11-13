a76107c70d171b67204c3a9d6c93a227ffb4cc85
Kupasva <130740583+Kupasva@users.noreply.github.com>
2023-11-13 12:26:42 +0530

    file_organizer

--- main.py
+++ main.py
@@ -1,45 +1,34 @@
-import tkinter as tk
+import os
+import shutil
 
-def button_click(button_number):
-    print(f"Button {button_number} clicked!")
+def organize_files(source_folder):
+    # Create a dictionary to map file extensions to their respective folders
+    extensions_folders = {
+        'txt': 'TextFiles',
+        'pdf': 'PDFs',
+        'jpg': 'Images',
+        'png': 'Images',
+        'xlsx': 'Spreadsheets',
+        # Add more file types and their corresponding folders as needed
+    }
 
-# Create the main window
-window = tk.Tk()
+    # Create folders for each file type
+    for folder in extensions_folders.values():
+        os.makedirs(os.path.join(source_folder, folder), exist_ok=True)
 
-# Set the window title
-window.title("Four Buttons Example")
+    # Organize files
+    for filename in os.listdir(source_folder):
+        if os.path.isfile(os.path.join(source_folder, filename)):
+            file_extension = filename.split('.')[-1]
+            if file_extension in extensions_folders:
+                source_path = os.path.join(source_folder, filename)
+                destination_folder = extensions_folders[file_extension]
+                destination_path = os.path.join(source_folder, destination_folder, filename)
 
-# Set the window size
-window.geometry("400x100")
+                # Move the file to its respective folder
+                shutil.move(source_path, destination_path)
+                print(f"Moved {filename} to {destination_folder} folder.")
 
-def front():
-    def File():
-        button_click(1)
-
-    def Edit():
-        button_click(2)
-
-    def View():
-        button_click(3)
-
-    def More():
-        button_click(4)
-
-    # Create buttons outside the inner functions
-    button1 = tk.Button(window, text="Button 1", command=File)
-    button1.grid(row=0, column=0, padx=20)
-
-    button2 = tk.Button(window, text="Button 2", command=Edit)
-    button2.grid(row=0, column=1, padx=10)
-
-    button3 = tk.Button(window, text="Button 3", command=View)
-    button3.grid(row=0, column=2, padx=10)
-
-    button4 = tk.Button(window, text="Button 4", command=More)
-    button4.grid(row=0, column=3, padx=10)
-
-# Call the front function
-front()
-
-# Run the Tkinter event loop
-window.mainloop()
+# Replace 'source_folder' with the path to the directory you want to organize
+source_folder = 'C:/Users/acer/OneDrive/Desktop/automate'
+organize_files(source_folder)
